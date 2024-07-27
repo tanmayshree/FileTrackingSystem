0cddc388157e2e68fe88e632054b574d1ce19d46
Tanmay Shree <tanmay.shree@rdash.io>
2024-07-28 00:38:15 +0530

    some changes

--- composeApp/src/androidMain/kotlin/com/filetrackingsystem/fts/Test.kt
+++ composeApp/src/androidMain/kotlin/com/filetrackingsystem/fts/Test.kt
@@ -1,15 +1,12 @@
 package com.filetrackingsystem.fts
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -28,7 +25,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
